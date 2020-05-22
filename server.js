473ac79ef7874942dc02cb6ea00063b7fca156e9
Zihao1010 <47839309+Zihao1010@users.noreply.github.com>
2020-05-22 21:10:30 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,62 @@
+console.log("In server.js!");
+
+// init project
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Establish a connection with the Mongo Database
+// Get the username, password, host, and databse from the .env file
+const mongoDB = ("mongodb+srv://"+
+                 process.env.USERNAME+
+                 ":"
+                 +process.env.PASSWORD+
+                 "@"
+                 +process.env.HOST+
+                 "/"
+                 +process.env.DATABASE);
+// console.log("Connection String: "+mongoDB);
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, retryWrites: true});
+
+//debugging 
+mongoose.connection.on('connected', function (){
+  console.log('Mongoose connected to '+process.env.DATABASE);
+});
+
+mongoose.connection.on('error', function (err){
+  console.log('Mongoose connection error: '+err);
+});
+
+mongoose.connection.on('disconnected', function (){
+  console.log('Mongoose disconnected.');
+});
+
+//start express 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set the view engine
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views/");
+
+//app.use(express.static(__dirname +'/public/'));
+app.use(express.static('public'));
+
+
+// Load routes
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+const adminRouter = require("./routes/admin");
+app.use("/admin", adminRouter);
+app.engine("html", require("ejs").renderFile);
+
+const apiRouter = require("./routes/user");
+app.use("/user", apiRouter);
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
